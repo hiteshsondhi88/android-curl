6f9a227c2b080fa0c3afdc9336d7ac8ace4f1c4f
Guillermo Orellana <guillermo.orellana@coniq.com>
2015-10-06 15:24:38 +0100

    Update settings.sh to support OSX

--- settings.sh
+++ settings.sh
@@ -5,5 +5,14 @@ ANDROID_NDK_ROOT_PATH=~/Android/ndk
 ANDROID_API_VERSION=9
 NDK_TOOLCHAIN_ABI_VERSION=4.8
 
-NUMBER_OF_CORES=$(nproc)
+NUMBER_OF_CORES=1
 HOST_UNAME=$(uname -m)
+
+OS=$(uname)
+
+if [[ $OS == 'Linux' ]]; then
+  NUMBER_OF_CORES=$(nproc)
+elif [[ $OS == 'Darwin' ]]; then # Assume Mac OS X
+  NUMBER_OF_CORES=$(sysctl hw.ncpu | awk '/hw.ncpu/{ print $2}{next;}')
+fi
+
